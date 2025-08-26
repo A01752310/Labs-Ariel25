d5dc7145a3ed1661f6c748df83eb60c5482d88d0
A01752310 <A01752310@tec.mx>
2025-08-26 10:00:59 -0600

    Imagen Scarlet e Integración Primer Script

---
+++ extract_hidden_images.py
@@ -0,0 +1,9 @@
+#----------------------------------------------------------
+# Lab #1: Steganography
+# Image processing through bit manipulation.
+#
+# Date: 1-Sep-2025
+# Authors:
+#           A01752310 Luis Ángel Godínez González
+#           A017
+#----------------------------------------------------------
